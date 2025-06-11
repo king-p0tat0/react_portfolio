ff5f80eba1af29455e94441a1b159f211d4da68b
king-p0tat0 <wjdtjdbs2089@gmail.com>
2025-06-12 06:52:31 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '@/layouts/header/Header.jsx'
+import MainPage from '@/pages/main/MainPage.jsx'
+
+function App() {
+  return (
+    <div>
+        <Header />
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './header.css';
+import '@fontsource/mogra';
+import '@fontsource/radley';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <nav className="sidebar-wrap">
+          <ul>
+            <li><a href="#top">Home</a></li>
+            <li><a href="#profile">Profile</a></li>
+            <li><a href="#skills">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+          </ul>
+        </nav>
+
+      </header>
+    </>
+  );
+};
+
+export default Header;
